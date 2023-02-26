024cf651a65966bced09b8460e10e41b84eded43
Koc-Ali <1koc.ali@gmail.com>
2023-02-26 20:15:29 +0100

    Initial commit

---
+++ task_1/2021_advent_coding_2.py
@@ -0,0 +1,36 @@
+import os
+
+# file handling
+fileDir = os.path.dirname(os.path.realpath('__file__'))
+
+# For accessing the file in the same folder
+filename = "2021_advent_1.input.txt"
+#filename = "2021_advent_1.input.sample.txt"
+
+# filename = "2022_advent_10.input.sample.txt"
+# filename = "2022_advent_10.input.sample2.txt"
+
+path_to_file = fileDir + '/' + filename
+
+file = open(os.path.expanduser(path_to_file))
+
+# problem solution
+amount_of_increases = 0
+
+height_values = []
+
+for line in file:
+    fields = line.strip().split()
+    current_depth = int(fields[0])
+
+    height_values.append(current_depth)
+
+for i in range(len(height_values)):
+    if i+3 < len((height_values)):
+        Sum1 = height_values[i]+height_values[i+1]+height_values[i+2]
+        Sum2 = height_values[i+1]+height_values[i+2]+height_values[i+3]
+
+        if Sum2 > Sum1:
+            amount_of_increases += 1
+
+print(amount_of_increases)
---
+++ task_2/2021_advent_coding_1.py
@@ -0,0 +1,29 @@
+import os
+
+# file handling
+fileDir = os.path.dirname(os.path.realpath('__file__'))
+
+# For accessing the file in the same folder
+filename = "2021_advent_1.input.txt"
+# filename = "2021_advent_1.input.sample.txt"
+
+# filename = "2022_advent_10.input.sample.txt"
+# filename = "2022_advent_10.input.sample2.txt"
+
+path_to_file = fileDir + '/' + filename
+
+file = open(os.path.expanduser(path_to_file))
+
+# problem solution
+last_depth = 0
+amount_of_increases = -1
+
+for line in file:
+    fields = line.strip().split()
+    current_depth = int(fields[0])
+
+    if current_depth > last_depth:
+        amount_of_increases += 1
+    last_depth = current_depth
+
+print(amount_of_increases)
